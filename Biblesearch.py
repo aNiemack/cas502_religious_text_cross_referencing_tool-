472804eb4a0510ff2643dfc642f80ff1b48736af
Joshua Carlin <Jcarlin3@asu.edu>
2025-02-16 17:05:05 -0700

    Adding Test code and function to be tested

---
+++ Biblesearch.py
@@ -0,0 +1,14 @@
+import re
+import EnglishESVBible.xml
+
+text = EnglishESVBible.xml
+word = input()
+
+	def search(text, word):
+		if(word in text):
+			print(count(word in text))
+		else:
+			print("Word not found"
+
+	print(search(text,word))
+
